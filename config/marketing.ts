a3f19460562a9266cc5c38b791806ee197d0bf10
Alextxnk <alexsolov766@gmail.com>
2023-04-13 18:34:53 +0300

    first commit

---
+++ config/marketing.ts
@@ -0,0 +1,40 @@
+import { MarketingConfig } from 'types';
+
+export const marketingConfig: MarketingConfig = {
+   mainNav: [
+      {
+         title: 'Блог',
+         href: '/blog',
+      },
+      {
+         title: 'Пользователи',
+         href: '/users',
+      },
+      {
+         title: 'Документация',
+         href: '/docs'
+      }
+      /* {
+         title: 'Features',
+         href: '/features',
+         disabled: true
+      },
+      {
+         title: 'Pricing',
+         href: '/pricing'
+      },
+      {
+         title: 'Blog',
+         href: '/blog'
+      },
+      {
+         title: 'Documentation',
+         href: '/docs'
+      },
+      {
+         title: 'Contact',
+         href: '/contact',
+         disabled: true
+      } */
+   ]
+};
